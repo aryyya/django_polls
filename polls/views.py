a6c33d8bd7ef425dd2272739a4e1d900e809321e
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-03-03 16:09:41 -0800

    Add ability to vote on question.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 def index(request):
     question_list = Question.objects.order_by('-pub_date')
@@ -15,7 +16,22 @@ def detail(request, question_id):
     })
 
 def results(request, question_id):
-    return HttpResponse(f'You are looking at the results of question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        'question': question
+    })
 
 def vote(request, question_id):
-    return HttpResponse(f'You are voting on question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice_id = request.POST['choice']
+        selected_choice = question.choice_set.get(pk=choice_id)
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You didn\'t select a choice.'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
